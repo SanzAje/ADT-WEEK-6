a39233f4116adea49e7f8442d38600d44c11b053
Amsterdam4Ever <153609998+SanzAje@users.noreply.github.com>
2025-03-21 10:32:01 +0700

    stack.c

--- stack.c
+++ stack.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include "stack.h"
 
-// Inisialisasi Stack
+
 void init(Stack *s) {
     s->top = -1;
 }
 
-// Mengecek apakah stack kosong
+
 int isEmpty(Stack *s) {
     return s->top == -1;
 }
 
-// Mengecek apakah stack penuh
+
 int isFull(Stack *s) {
     return s->top == MAX - 1;
 }
 
-// Menambahkan elemen ke stack
+
 void push(Stack *s, int value) {
     if (isFull(s)) {
         printf("Stack Penuh!\n");
@@ -25,7 +25,7 @@ void push(Stack *s, int value) {
     s->array[++s->top] = value;
 }
 
-// Menghapus elemen dari stack
+
 int pop(Stack *s) {
     if (isEmpty(s)) {
         printf("Stack Kosong!\n");
